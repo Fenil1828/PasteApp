627075acc41eb2b29a8e5a2f2e1ea29636bc6bd7
Fenil1828 <jasanifenil18@gmail.com>
2025-04-28 19:36:38 +0530

    Update Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,19 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
+
+    <div>
+
+      <div className='font-bold border-b-1 border-gray-800 mb-5 pb-2'>
+        V Developers
+      </div>
+
     <div className='relative flex flex-row items-center  gap-4 place-content-evenly mb-5 overflow-visible max-w-[300px] ml-[100px] 
     p-3 rounded-2xl bg-gray-300 shadow-md shadow-gray
     '>
+
+
+
       <NavLink
       to="/"
        className={({isActive}) => 
@@ -30,7 +40,9 @@ const Navbar = () => {
         Pastes
       </NavLink>
     </div>
+    </div>
   )
 }
+
 
 export default Navbar

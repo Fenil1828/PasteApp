bc0b8a65e040c82e39b5cec55f82f3aa26d63c9a
Fenil1828 <jasanifenil18@gmail.com>
2025-04-28 20:22:22 +0530

    Restore original code and remove deployment fixes

--- src/main.jsx
+++ src/main.jsx
@@ -4,27 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import { store } from './store.js'
 import { Provider } from 'react-redux'
-// We'll use dynamic import instead
-// import {Toaster} from 'react-hot-toast';
+import {Toaster} from 'react-hot-toast';
 
-// Initial render without Toaster
-const root = createRoot(document.getElementById('root'));
-root.render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>
-);
-
-// Dynamically import Toaster
-import('react-hot-toast').then(({ Toaster }) => {
-  root.render(
-    <StrictMode>
-      <Provider store={store}>
-        <App />
-        <Toaster />
-      </Provider>
-    </StrictMode>
-  );
-});
+    <Provider store={store} >
+    <App />
+    <Toaster/>
+    </ Provider>
+  </StrictMode>,
+)
